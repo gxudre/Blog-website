8d4842a3c2c06474f46841950fd9c044b9d93df3
Gustavo Xudré <gxudre@gmail.com>
2024-09-27 01:21:17 -0300

    Close #14, Close #9, Close #11, Close #10, Close #28, Close #13

--- react-blog-website/src/main.jsx
+++ react-blog-website/src/main.jsx
@@ -2,9 +2,49 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import Home from './pages/Home.jsx';
+import Blogs from './pages/Blogs.jsx';
+import About from './pages/About.jsx';
+import Contact from './pages/Contact.jsx';
+import Services from './pages/Services.jsx';
+import Erro404 from './pages/Erro404.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    children: [
+      {
+        path: "/",
+        element: <Home/>
+      },
+      {
+        path: "/blogs",
+        element: <Blogs/>
+      },
+      {
+        path: "/about",
+        element: <About/>
+      },
+      {
+        path: "/contact",
+        element: <Contact/>
+      },
+      {
+        path: "/services",
+        element: <Services/>
+      },
+      {
+        path: "*",
+        element: <Erro404/>
+      }
+    ]
+  },
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
---
+++ react-blog-website/src/pages/Erro404.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Erro404 = () => {
+  return (
+    <div>Erro 404. Página não encontrada!</div>
+  )
+}
+
+export default Erro404
